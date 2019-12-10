e4e75f00ed39f286a0d6356c4d063d3cfe7c84f9
Jeffrey Marshall <m.jeffrey.marshall@gmail.com>
2019-12-09 22:08:29 -0600

    everything works

---
+++ src/store.js
@@ -0,0 +1,16 @@
+const bookmarks = [
+  {
+    id: 1,
+    title: 'YouTube',
+    url: 'https://www.youtube.com/',
+    rating: 5
+  },
+  {
+    id: 2,
+    title: 'Twitch',
+    url: 'https://www.twitch.tv/',
+    rating: 1
+  }
+];
+
+module.exports = bookmarks;
