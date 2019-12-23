f76b425976da8c8612995b621aa24af7df012913
Jeffrey Marshall <m.jeffrey.marshall@gmail.com>
2019-12-23 21:40:59 +0000

    testing

--- src/store.js
+++ src/store.js
@@ -1,15 +1,24 @@
 const bookmarks = [
   {
     id: 1,
-    title: 'YouTube',
-    url: 'https://www.youtube.com/',
+    title: 'Thinkful',
+    url: 'https://www.thinkful.com',
+    description: 'Think outside the classroom',
     rating: 5
   },
   {
     id: 2,
-    title: 'Twitch',
-    url: 'https://www.twitch.tv/',
-    rating: 1
+    title: 'Google',
+    url: 'https://www.google.com',
+    description: 'Where we find everything else',
+    rating: 4
+  },
+  {
+    id: 3,
+    title: 'MDN',
+    url: 'https://developer.mozilla.org',
+    description: 'The only place to find web documentation',
+    rating: 5
   }
 ];
 
